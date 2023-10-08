5e0023c8ee9993cc879c916e79749edfb560c44d
Ivan Zorin <creator@localzet.com>
2023-10-09 02:51:38 +0300

    Update Frame.php

--- src/Server/Protocols/Frame.php
+++ src/Server/Protocols/Frame.php
@@ -6,12 +6,12 @@
  *
  * @author      Ivan Zorin <creator@localzet.com>
  * @copyright   Copyright (c) 2018-2023 Localzet Group
- * @license     https://www.gnu.org/licenses/agpl AGPL-3.0 license
+ * @license     https://www.gnu.org/licenses/agpl-3.0 GNU Affero General Public License v3.0
  *
  *              This program is free software: you can redistribute it and/or modify
- *              it under the terms of the GNU Affero General Public License as
- *              published by the Free Software Foundation, either version 3 of the
- *              License, or (at your option) any later version.
+ *              it under the terms of the GNU Affero General Public License as published
+ *              by the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
  *
  *              This program is distributed in the hope that it will be useful,
  *              but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,6 +20,8 @@
  *
  *              You should have received a copy of the GNU Affero General Public License
  *              along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *              For any questions, please contact <creator@localzet.com>
  */
 
 namespace localzet\Server\Protocols;
@@ -30,45 +32,51 @@ use function substr;
 use function unpack;
 
 /**
- * Frame Protocol.
+ * Протокол Frame.
  */
 class Frame
 {
     /**
-     * Check the integrity of the package.
+     * Проверка целостности пакета.
      *
      * @param string $buffer
      * @return int
      */
     public static function input(string $buffer): int
     {
+        // Если длина буфера меньше 4, возвращаем 0.
         if (strlen($buffer) < 4) {
             return 0;
         }
+        // Распаковываем данные из буфера.
         $unpackData = unpack('Ntotal_length', $buffer);
+        // Возвращаем общую длину.
         return $unpackData['total_length'];
     }
 
     /**
-     * Decode.
+     * Декодирование.
      *
      * @param string $buffer
      * @return string
      */
     public static function decode(string $buffer): string
     {
+        // Возвращаем подстроку буфера, начиная с 4-го символа.
         return substr($buffer, 4);
     }
 
     /**
-     * Encode.
+     * Кодирование.
      *
      * @param string $data
      * @return string
      */
     public static function encode(string $data): string
     {
+        // Общая длина равна 4 плюс длина данных.
         $totalLength = 4 + strlen($data);
+        // Возвращаем упакованные данные.
         return pack('N', $totalLength) . $data;
     }
 }
