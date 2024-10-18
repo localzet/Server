37e586b6149e5e50ea08699ab9dde910188924a8
Ivan Zorin <creator@localzet.com>
2024-10-19 00:45:32 +0300

    Update rector.php

--- rector.php
+++ rector.php
@@ -3,11 +3,28 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
     ])
-    // uncomment to reach your current PHP version
     ->withPhpSets(php81: true)
-    ->withTypeCoverageLevel(0);
+    ->withSets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::PHP_81,
+
+//        // SetList::PHP_POLYFILLS,
+//        SetList::CODE_QUALITY,
+//        SetList::CODING_STYLE,
+//        SetList::DEAD_CODE,
+//        // SetList::STRICT_BOOLEANS,
+//        // SetList::NAMING,
+//        // SetList::RECTOR_PRESET,
+//        SetList::PRIVATIZATION,
+//        SetList::TYPE_DECLARATION,
+//        // SetList::EARLY_RETURN,
+//        // SetList::INSTANCEOF,
+//        // SetList::CARBON,
+    ]);
